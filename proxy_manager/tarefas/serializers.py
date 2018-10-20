6e8728aab6df1c10ca82981fa3e69616da4503ef
eduardo <eduardo.porfiro.costa.1998@gmail.com>
2018-10-20 14:14:35 -0300

    Change_Tarefas

--- proxy_manager/tarefas/serializers.py
+++ proxy_manager/tarefas/serializers.py
@@ -1,12 +1,52 @@
 from rest_framework import serializers
 from tarefas.models import Dispositivo, Dado
+from core.models import Task,Atuador_troca_estado, Atuador_boolean,\
+    If_sensor_string,If_sensor_numero, If_sensor_dadosensor
+
 
 class DispositivoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dispositivo
         fields = '__all__'
 
+
 class DadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dado
+        fields = '__all__'
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
+
+
+class Atuador_troca_estadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Atuador_troca_estado
+        fields = '__all__'
+
+
+class Atuador_booleanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Atuador_boolean
+        fields = '__all__'
+
+
+class If_sensor_stringSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = If_sensor_string
+        fields = '__all__'
+
+
+class If_sensor_numeroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = If_sensor_numero
+        fields = '__all__'
+
+
+class If_sensor_dadosensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = If_sensor_dadosensor
         fields = '__all__'
--- proxy_manager/tarefas/urls.py
+++ proxy_manager/tarefas/urls.py
@@ -5,5 +5,28 @@ urlpatterns = [
     path('dispositivo/', views.DispositivoList.as_view(), name='dispositivo_list'),
     path('<int:id>/dispositivoUpdate/', views.DispositivoUpdateDelete.as_view(), name='dispositivo_update'),
 
+    path('task/', views.TaskList.as_view(), name='task_list'),
+    path('<int:id>/taskUpdate/', views.TaskUpdateDelete.as_view(), name='task_update'),
+
+    path('atuador_troca_estado/', views.Atuador_booleanList.as_view(), name='atuador_troca_estado'),
+    path('<int:id>/atuador_troca_estadoUpdate/', views.Atuador_troca_estadoUpdateDelete.as_view(),
+         name='atuador_troca_estado_update'),
+
+    path('atuador_boolean/', views.Atuador_booleanList.as_view(), name='atuador_boolean'),
+    path('<int:id>/atuador_booleanUpdate/', views.Atuador_troca_estadoUpdateDelete.as_view(),
+         name='atuador_boolean_update'),
+
+    path('if_sensor_numero/', views.If_sensor_numeroList.as_view(), name='if_sensor_numero'),
+    path('<int:id>/if_sensor_numero/', views.If_sensor_numeroUpdateDelete.as_view(),
+         name='if_sensor_numero_update'),
+
+    path('if_sensor_string/', views.If_sensor_stringList.as_view(), name='if_sensor_string'),
+    path('<int:id>/if_sensor_string/', views.If_sensor_stringUpdateDelete.as_view(),
+         name='if_sensor_string_update'),
+
+    path('if_sensor_dadosensor/', views.If_sensor_dadosensorList.as_view(), name='if_sensor_dadosensor'),
+    path('<int:id>/if_sensor_dadosensor/', views.If_sensor_dadosensorUpdateDelete.as_view(),
+         name='if_sensor_dadosensor_update'),
+
     path('dado/', views.DadoList.as_view(), name='dadolist'),
 ]
