325f780e06301ace0ae06e062cc374f94a1d89e2
eduardo <eduardo.porfiro.costa.1998@gmail.com>
2018-10-22 10:50:06 -0300

    Change_Tarefas

--- proxy_manager/core/admin.py
+++ proxy_manager/core/admin.py
@@ -1,3 +1,9 @@
+from .models import *
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Celery)
+admin.site.register(Task)
+admin.site.register(If_sensor_numero)
+admin.site.register(If_sensor_string)
+admin.site.register(If_sensor_dadosensor)
+admin.site.register(If_sensor_boolean)
--- proxy_manager/tarefas/admin.py
+++ proxy_manager/tarefas/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from tarefas.models import Dado, Dispositivo
+from tarefas.models import Dado, Dispositivo, Atuador_troca_estado, Atuador_boolean, Task, Job
 
-admin.site.register(Dado)
 admin.site.register(Dispositivo)
+admin.site.register(Dado)
+admin.site.register(Job)
+admin.site.register(Atuador_troca_estado)
+admin.site.register(Atuador_boolean)
 
